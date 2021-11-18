6eb4179faa646ca54d958c9c9759d1b366ca06c7
jackmleitch <jackmleitch@gmail.com>
2021-11-18 15:27:26 -0700

    project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="churn_model",
+    packages=["src"],
+    version="0.1.0",
+    description="Customer churn model on telecom data",
+    author="JackLeitch",
+    license="MIT",
+)
